c2d329e54c492b9ea5b4bef2d9daa91bb8846cfc
NickCostley <34405392+NickCostley@users.noreply.github.com>
2025-05-20 14:49:29 -0700

    Update LED_Multiplexer_No_Shift_Reg.ino
    Finished Project

--- LED_Multiplexer_No_Shift_Reg.ino
+++ LED_Multiplexer_No_Shift_Reg.ino
@@ -25,13 +25,9 @@ bool On[8][6] = {
 // A0 A1 A2 A3 A4 A5 2 3 4 5 6 7 8 9 10 11 12 13
 // 14 15 16 17 18 19
 
-const int SER = 12;
-const int LATCH = 11;
-const int CLK = 13;
-const int column[6] = {14, 15, 16, 17, 18, 19};
-const int row[8] = {6, 7, 8, 9, 10, 11, 12, 13}
+const int column[6] = {14, 15, 16, 17, 18, 19}; // Cathodes (negative) / transistors 14(A0) = 
+const int row[8] = {6, 7, 8, 9, 10, 11, 12, 13}; // Anodes (positive) 6 = top, 13 = bottom of board
 
-int bin = 0;
 
 void setup() {
   for (int i = 6; i < 14; i++){
@@ -43,32 +39,52 @@ void setup() {
 }
 
 void loop() {
-
+  display();
 }
+
+int disp[8][6] = {
+{1, 0, 1, 0, 1, 0},
+{0, 1, 0, 1, 0, 1},
+{1, 0, 1, 0, 1, 0},
+{0, 1, 0, 1, 0, 1},
+{1, 0, 1, 0, 1, 0},
+{0, 1, 0, 1, 0, 1},
+{1, 0, 1, 0, 1, 0},
+{0, 1, 0, 1, 0, 1}
+};
+
+bool disp2[8][6] = {
+{1, 1, 1, 1, 1, 1},
+{1, 1, 1, 1, 1, 1},
+{1, 1, 1, 1, 1, 1},
+{1, 1, 1, 1, 1, 1},
+{1, 1, 1, 1, 1, 1},
+{1, 1, 1, 1, 1, 1},
+{1, 1, 1, 1, 1, 1},
+{1, 1, 1, 1, 1, 1}
+};
 
 
 void display(){
   for (int f = 0; f < 1000; f++){
-    for (int i = 2; i < 8; i++){      // Loops through different transistors being active.
-      digitalWrite(LATCH, LOW);       // All anodes are set low to prevent unintentional activation.
-      for (int j = 0; j < 8; j++){
-        if (a[j][i-2] == 1){          // 'If pixel is high'
-          digitalWrite(LATCH, HIGH);
-          digtalWrite(i, HIGH);
-          delay(100);
-          digitalWrite(i, LOW);
-          bin = pow(2,j);
-          shiftOut(SER, CLK, MSBFIRST, bin);
+    for (int i = 0; i < 8; i++){
+      digitalWrite(row[i], HIGH);   // Turn on this row
+      for (int j = 0; j < 6; j++){
+        if (disp[i][j] == 1){          // 'If pixel is high' 
+          digitalWrite(column[j], HIGH);  // Turn on a transistor
+          delay(1);
         }
         else {
-          digitalWrite(i, LOW);
-          digitalWrite(LATCH, LOW);
+          digitalWrite(column[j], LOW);
         }
       }
-      digitalWrite(i, LOW);
+      digitalWrite(row[i], LOW);
+      //delay(1);
+      for (int j = 0; j < 6; j++){
+        digitalWrite(column[j], LOW);
+      }
     }
   }
 }
 
-// Transistor on, Latch off.
-// Shift in serial bits, Latch on
+
